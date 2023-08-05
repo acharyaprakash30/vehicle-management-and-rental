34c72e1b85e7779860d4e2dad4db655805fe7f21
acharyaprakash30 <acharyaprakash030@gmail.com>
2023-08-05 18:05:10 +0545

    first-commit

---
+++ frontend/src/Helper/Private.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+// import axios from "axios";
+import Http from "./Http";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import PrivateSpinner from "./PrivateSpinner";
+const Private = ({ children }) => {
+  const { userToken } = useSelector((state) => state.auth);
+  const navigate = useNavigate();
+  const [ok, setOk] = useState(false);
+
+  useEffect(() => {
+    const authCheck = async () => {
+      try {
+        const res = await Http.get(
+          "/api/v1/auth/admin-auth"
+        );
+        if (!res.data.ok) {
+          setTimeout(() => {
+            navigate("/");
+          }, 1000);
+        } else {
+          setOk(true);
+        }
+      } catch (error) {}
+    };
+    if (userToken) authCheck();
+  }, [userToken]);
+  return ok ? <>{children}</> : <PrivateSpinner />;
+};
+
+export default Private;
